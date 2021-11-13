e40a72980181375f216f9648d9b4c37bb531c584
Satish Junghare <satish.junghare@ewebcore.net>
2021-11-13 17:33:18 +0530

    -

---
+++ max_sum_subarray.py
@@ -0,0 +1,11 @@
+'''
+I/P : 1, -3, 2, -5, 7
+O/P :
+
+Input will have possitive or negative values. You have to calculate sum of any contigious numbers from an array and find the maximum sum given by multiple calculations.
+'''
+
+# Bruite force approach
+arr = [1, -3, 2, -5, 7]
+
+for i in arr:
