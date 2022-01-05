f0095859bf93e942b53c2bf7e4dd5e42bff630b2
Satish Junghare <satish8264@gmail.com>
2022-01-05 08:39:48 +0530

    calculate max sum of sub arrays, reverse string and array, return frequency of chars in string

--- max_sum_subarray.py
+++ max_sum_subarray.py
@@ -8,4 +8,37 @@ Input will have possitive or negative values. You have to calculate sum of any c
 # Bruite force approach
 arr = [1, -3, 2, -5, 7]
 
-for i in arr:
+maxSum = arr[0]
+for i in range(0, len(arr)):
+    for j in range(0, len(arr)):
+        if i > j:
+            continue
+
+        currSum = 0
+        for k in range(i, j+1):
+            currSum += arr[k]
+        
+        if currSum > maxSum:
+            maxSum = currSum
+print(maxSum)
+
+
+# Optimal approach
+maxSum = sum = 0
+maxNeg = arr[0]
+for i in range(0, len(arr)):
+    if sum+arr[i] > 0:
+        sum+=arr[i]
+    else:
+        sum = 0
+    
+    if sum > maxSum:
+        maxSum = sum
+    
+    if arr[i] > maxNeg:
+        maxNeg = arr[i]
+
+if maxSum == 0:
+    print(maxNeg)
+else:
+    print(maxSum)
